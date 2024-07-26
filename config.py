6311c38682eafd11b7f8cac29eb8493f03c7b222
Homailson <homailson@gmail.com>
2024-07-26 12:12:41 -0300

    preparing to production

--- config.py
+++ config.py
@@ -4,26 +4,26 @@ from datetime import timedelta
 
 
 # Local
-class Config:
-    MONGO_URI = os.getenv('MONGO_URI')
-    MONGO_DBNAME = 'SchoolCare'
-    SECRET_KEY = 'uma bela senha'
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
-
-
-# On render.com
 # class Config:
 #     MONGO_URI = os.getenv('MONGO_URI')
-#     MONGO_DBNAME = 'SchoolWarden'
-#     SECRET_KEY = os.getenv('SECRET_KEY')
+#     MONGO_DBNAME = 'SchoolCare'
+#     SECRET_KEY = 'uma bela senha'
 #     MAIL_SERVER = 'smtp.gmail.com'
 #     MAIL_PORT = 587
 #     MAIL_USE_TLS = True
 #     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
 #     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 #     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+
+
+# On render.com
+class Config:
+    MONGO_URI = os.getenv('MONGO_URI')
+    MONGO_DBNAME = 'SchoolWarden'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
